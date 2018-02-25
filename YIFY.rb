2bbbcbbf5ded50200df31c66e9dffaf1cbc5e0fc
Rishav Rungta <rishav394@gmail.com>
2018-02-25 12:59:29 +0530

    More Interactive
    * Many new features
    * Bug fixes
    * Improved exception handling

--- YIFY.rb
+++ YIFY.rb
@@ -1,10 +1,6 @@
-require "rubygems"
-require "json"
-require 'restclient'
-require 'crack'
-require 'io/console'
-require "fileutils"
-require 'clipboard'
+require 'restclient'	# => To open urls
+require 'crack'			# => To parse string JSON
+require 'clipboard'		# => To copy magnet link to Clipboard
 
 reset = 1
 while reset!=0
@@ -15,39 +11,48 @@ while reset!=0
 	if data["data"]["movie_count"]!=0
 		i=0
 		while i<data["data"]["movie_count"] && i!=20
-			puts "#{i+1}. #{data["data"]["movies"][i]["title"]}"#+"\t\t\t"+data["data"]["movies"][i]["torrents"][0]["hash"]
+			puts "#{i+1}. #{data["data"]["movies"][i]["title_long"]}"	# => Long title also shows the date of release
 			i+=1
 		end
-		puts "0/ENTER. Reset"
+		puts "0/ENTER- Reset"
+		puts "Enter your choice: "
 		inp=gets
 		inp=inp.to_i
-		if inp>0
-			TITLE=data["data"]["movies"][inp-1]["title"]
-			i=0
-			while i<data["data"]["movie_count"]
-				if data["data"]["movies"][inp-1]["torrents"][i]["quality"]=="720p"
-					break
-				end
-				i+=1
+		if inp>0 && inp<20
+			quality_count=0
+			puts
+			data["data"]["movies"][inp-1]["torrents"].each do
+				puts "#{quality_count+1}. #{data["data"]["movies"][inp-1]["torrents"][quality_count]["quality"]}"
+				quality_count=quality_count+1
 			end
-			TORRENT_HASH=data["data"]["movies"][inp-1]["torrents"][i]["hash"]
-			MLINK="magnet:?xt=urn:btih:#{TORRENT_HASH}&dn=ez&tr=udp%3A%2F%2Ftracker.openbittorrent.com%3A80&tr=udp%3A%2F%2Fopentor.org%3A2710&tr=udp%3A%2F%2Ftracker.ccc.de%3A80&tr=udp%3A%2F%2Ftracker.blackunicorn.xyz%3A6969&tr=udp%3A%2F%2Ftracker.coppersurfer.tk%3A6969&tr=udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969"
-			puts MLINK
-			Clipboard.copy(MLINK)
-			puts "\nCopied to clipboard successfully. Press Ctrl+V to paste. "
-			xfile=File.open("Magnet.txt","w")
-			xfile.write(MLINK)
-			xfile.close
-			reset = 0
+			puts "Enter quality\n"
+			i=gets
+			i=i.to_i
+			puts
+			if i<1 || i>quality_count  # => quality range <=3 >1
+				puts "Please Enter the correct number of the desired quality."
+				puts "Resetting...\n"
+			else
+				TITLE=data["data"]["movies"][inp-1]["title_long"]		# => Long title also shows the date of release
+				TORRENT_HASH=data["data"]["movies"][inp-1]["torrents"][i-1]["hash"]
+				MLINK="magnet:?xt=urn:btih:#{TORRENT_HASH}&dn=ez&tr=udp%3A%2F%2Ftracker.openbittorrent.com%3A80&tr=udp%3A%2F%2Fopentor.org%3A2710&tr=udp%3A%2F%2Ftracker.ccc.de%3A80&tr=udp%3A%2F%2Ftracker.blackunicorn.xyz%3A6969&tr=udp%3A%2F%2Ftracker.coppersurfer.tk%3A6969&tr=udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969"
+				puts MLINK
+				Clipboard.copy(MLINK)
+				puts "\nCopied to clipboard successfully. Press Ctrl+V to paste. "
+				xfile=File.open("Magnet.txt","w")
+				xfile.write(TITLE)
+				xfile.write("\n")
+				xfile.write(MLINK)
+				xfile.close						
+				reset = 0
+			end
 		else
 			puts "Resetting...\n"
 		end
 	else
-		puts "Please Enter correct search query matching on: \nMovie Title/IMDb Code, Actor Name/IMDb Code, Director Name/IMDb Code\n"
+		puts "\nPlease Enter correct search query matching on: \nMovie Title/IMDb Code, Actor Name/IMDb Code, Director Name/IMDb Code\n"
 		puts "Resetting...\n"
 	end
 end
-
-
 puts "\nPress Enter to continue"
 gets
